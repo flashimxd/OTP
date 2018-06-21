b02ca21ce2bb6af89cc9e16bad63ab444ab38ebe
Rangel Netto <j.rangel.flash@gmail.com>
2018-06-21 11:31:19 +0100

    Update twilio.js

--- functions/twilio.js
+++ functions/twilio.js
@@ -1,6 +1,6 @@
 const twilio = require('twilio');
 
-const accountSid = 'ACcaa93db418acdda3d4bb16d910934da0';
-const authToken = '43b1fd463806f37bf00067f0d8486dde';
+const accountSid = 'Sid';
+const authToken = 'AuthToken';
 
 module.exports = new twilio.Twilio(accountSid, authToken);
